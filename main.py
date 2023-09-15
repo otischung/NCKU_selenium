24e367ecc2e247d547fe1f1a1f2cae779715d76c
otischung <my072814638@gmail.com>
2023-09-15 18:59:52 +0800

    add send captcha

--- main.py
+++ main.py
@@ -19,25 +19,39 @@ def main():
     
     # 開啟網頁
     driver.get("https://course.ncku.edu.tw/index.php")
+
     # 關閉 KUAP APP 訊息通知
     driver.find_element(By.CSS_SELECTOR, "#show_sp_msg .btn").click()
+
     # 點擊登入頁面
     # TODO: 頁面跳轉應可以加速
     driver.find_element(By.LINK_TEXT, "登入").click()
+
     # 儲存 Cookies
     cookies = driver.get_cookies()
+
     # 將 cookies 輸入至 session 裡面
     for c in cookies:
         sess.cookies.set(c["name"], c["value"])
+    
     # 輸入帳號
     driver.find_element(By.ID, "user_id").send_keys(env.login["username"])
+
     # 輸入密碼
     driver.find_element(By.ID, "passwd").send_keys(env.login["passwd"])
-    # 驗證碼
+
+    # 顯示驗證碼
     captcha_url = driver.find_element(By.CSS_SELECTOR, ".click").get_attribute("src")
     img = Image.open(sess.get(captcha_url, stream=True).raw)  # https://stackoverflow.com/questions/7391945/how-do-i-read-image-data-from-a-url-in-python
     img.show()
-    input()
+    print("Please input captcha result: ", end="")
+    captcha = input()
+
+    # 輸入驗證碼
+    driver.find_element(By.ID, "code").send_keys(str(captcha))
+
+    # 送出
+    driver.find_element(By.CSS_SELECTOR, "#submit_by_acpw > span").click()
 
     sess.close()
 
